ef546078f216e5d858ccd6f25544d82a4be7bd57
Prasanth496 <saiprashanthreddy96@gmail.com>
2020-05-01 12:46:34 +0530

    [prasanth]add. added base class User

--- app/src/main/java/com/example/fundooapplication/LoginActivity.kt
+++ app/src/main/java/com/example/fundooapplication/LoginActivity.kt
@@ -2,11 +2,25 @@ package com.example.fundooapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var  mTextEmail : EditText
+    lateinit var mTextPassword: EditText
+    lateinit var mButtonLogin : Button
+    lateinit var mTextViewRegister : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mTextEmail = findViewById(R.id.edittext_email)
+        mTextPassword = findViewById(R.id.edittext_password)
+        mButtonLogin = findViewById(R.id.button_login)
+        mTextViewRegister = findViewById(R.id.textview_register)
+
     }
 }
---
+++ app/src/main/java/com/example/fundooapplication/model/User.kt
@@ -0,0 +1,44 @@
+package com.example.fundooapplication.model
+
+class User {
+
+    private var id : Int = 0
+    private lateinit var name : String
+    private lateinit var email : String
+    private lateinit var password : String
+
+    fun getId(): Int {
+        return id
+    }
+
+    fun setId(id : Int) {
+        this.id = id
+    }
+
+    fun getName() : String {
+        return name
+    }
+
+    fun setname(name : String) {
+        this.name = name
+    }
+
+    fun getEmail() : String {
+        return email
+    }
+
+    fun setEmail(email : String) {
+        this.email = email
+    }
+
+    fun getPassword() : String {
+        return password
+    }
+
+    fun setPassword(password : String) {
+        this.password = password
+    }
+}
+
+
+}
